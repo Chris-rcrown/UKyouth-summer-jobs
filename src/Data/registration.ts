f5ecf6c0757bc182570be489b4c1c26f0e124594
Rotimi Crown <rotimiworkmail1@gmail.com>
2025-06-12 01:37:41 +0100

    registration form static state created

---
+++ src/Data/registration.ts
@@ -0,0 +1,99 @@
+export interface Registrant {
+    serialNumber: number;
+    fullName: string;
+    borough: string;
+    registrationDate: string;
+    gender: string;
+    placementStatus: string;
+}
+
+export const registrants: Registrant[] = [
+    {
+        serialNumber: 1,
+        fullName: "John Dukes",
+        borough: "Manchester",
+        registrationDate: "Feb 26, 2025",
+        gender: "Male",
+        placementStatus: "Pending"
+    },
+    {
+        serialNumber: 2,
+        fullName: "James Hall",
+        borough: "Manchester",
+        registrationDate: "Feb 26, 2025",
+        gender: "Male",
+        placementStatus: "Pending"
+    },
+    {
+        serialNumber: 3,
+        fullName: "Kenneth Allen",
+        borough: "Manchester",
+        registrationDate: "Feb 26, 2025",
+        gender: "Male",
+        placementStatus: "Pending"
+    },
+    {
+        serialNumber: 4,
+        fullName: "Jerry Helfer",
+        borough: "Manchester",
+        registrationDate: "Feb 26, 2025",
+        gender: "Male",
+        placementStatus: "Pending"
+    },
+    {
+        serialNumber: 5,
+        fullName: "Frances Swann",
+        borough: "Manchester",
+        registrationDate: "Feb 26, 2025",
+        gender: "Male",
+        placementStatus: "Pending"
+    },
+    {
+        serialNumber: 6,
+        fullName: "Kurt Bates",
+        borough: "Manchester",
+        registrationDate: "Feb 26, 2025",
+        gender: "Male",
+        placementStatus: "Pending"
+    },
+    {
+        serialNumber: 7,
+        fullName: "Katie Sims",
+        borough: "Manchester",
+        registrationDate: "Feb 26, 2025",
+        gender: "Male",
+        placementStatus: "Controlled Group"
+    },
+    {
+        serialNumber: 8,
+        fullName: "Daniel Hamilton",
+        borough: "Manchester",
+        registrationDate: "Feb 26, 2025",
+        gender: "Male",
+        placementStatus: "Controlled Group"
+    },
+    {
+        serialNumber: 9,
+        fullName: "Bradley Lawlor",
+        borough: "Manchester",
+        registrationDate: "Feb 26, 2025",
+        gender: "Male",
+        placementStatus: "Controlled Group"
+    },
+    {
+        serialNumber: 10,
+        fullName: "Autumn Phillips",
+        borough: "Manchester",
+        registrationDate: "Feb 26, 2025",
+        gender: "Male",
+        placementStatus: "Controlled Group"
+    },
+    {
+        serialNumber: 11,
+        fullName: "Dennis Callis",
+        borough: "Manchester",
+        registrationDate: "Feb 26, 2025",
+        gender: "Male",
+        placementStatus: "Controlled Group"
+    }
+];
--- src/pages/registration-records/RgstrnCompForm.tsx
+++ src/pages/registration-records/RgstrnCompForm.tsx
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import { useParams, } from 'react-router-dom';
-import { referrals,formQuestions } from '../../Data/referral'; 
+import { registrants } from '../../Data/registration';
+import { formQuestions } from '../../Data/referral'; 
 import DashboardLayout from '../../components/dashboard-layout';
 import { FooterNavigation } from '../../components/button';
 
-const ReferralForm: React.FC = () => {
+const RgstrnCompForm: React.FC = () => {
 
     const { id } = useParams<{ id: string }>(); 
-    const referral = referrals.find(r => r.id === Number(id));
+    const registrant = registrants.find(r => r.serialNumber === Number(id));
 
     const [currentPage, setCurrentPage] = useState(1);
 
@@ -29,69 +30,70 @@ const ReferralForm: React.FC = () => {
     }, [currentPage]);
 
     return (
-        <DashboardLayout pageName='Referral Records' >
+        <DashboardLayout pageName='Registration Records' >
             <div className="p-6 bg-gray-100 min-h-screen w-full">
                 <div className="bg-gray-100 shadow-md p-6 border-white rounded-2xl  mx-auto ">
             <div >
-                    <a href="/referrer-records" className="text-blue-500 flex items-center gap-1">
+                    <a href="/referrer-records" className="text-[#12BAE3] flex items-center gap-1">
                     <span>&lt;</span> Back
                     </a>
             </div>
-        <div className="flex justify-between items-center my-4">
-            {referral ? (
-                <div className="flex items-center gap-2">
-                    <span className="font-bold text-black">{referral?.fullName}</span>
-                    <span
-                        className={`
-                            px-4 py-1 rounded-full flex items-center space-x-2
-                            ${referral?.status === 'Pending' && 'bg-orange-100 text-orange-700 border border-orange-400'}
-                            ${referral?.status === 'Registered' && 'bg-green-100 text-green-700 border border-green-400'}
-                        `}
-                    >
+        <div className="flex  items-center justify-between my-4">
+            {registrant ? (
+                <div className="flex justify-between items-center  w-full">
+                    
+                    <div className="font-bold text-black">{registrant?.fullName}</div>
+                    <div className='flex font-semibold gap-4 items-center'>
+                        <span>Placement Status</span>            
                         <span
                             className={`
-                                w-3 h-3 rounded-full mr-2
-                                ${referral?.status === 'Pending' && 'bg-orange-500'}
-                                ${referral?.status === 'Registered' && 'bg-green-500'}
+                                px-4 py-1 rounded-full flex items-center space-x-2
+                                ${registrant?.placementStatus === 'Pending' && 'bg-orange-100 text-orange-700 border border-orange-400'}
+                                ${registrant?.placementStatus === 'Controlled Group' && 'bg-green-100 text-green-700 border border-green-400'}
                             `}
-                        ></span>
-                        <span>
-                            {referral?.status}
+                        >
+                            <span
+                                className={`
+                                    w-3 h-3 rounded-full mr-2
+                                    ${registrant?.placementStatus === 'Pending' && 'bg-orange-500'}
+                                    ${registrant?.placementStatus === 'Controlled Group' && 'bg-green-500'}
+                                `}
+                            ></span>
+                            <span>
+                                {registrant?.placementStatus}
+                            </span>
                         </span>
-                    </span>
+                    </div>
                 </div>
             ) : (
                 <div className="text-red-500">Referral not found</div>
             )}
-            <div className="flex gap-2">
-                <button className="border border-blue-300 text-blue-500 hover:bg-blue-200 px-4 py-1 rounded">Register</button>
-                <button className="border border-red-500 text-red-500 hover:bg-red-200 px-4 py-1 rounded">Deactivate Referral</button>
-            </div>
+            
         </div>    
 
         <div className="flex items-center justify-center bg-white py-3 px-3 gap-4 mb-4 rounded-2xl">
             <div className={`flex flex-col items-center ${currentPage === 1 ? 'font-bold' : 'font-normal'}`}>
-                <div className={`w-8 h-8 rounded-full ${currentPage === 1 ? 'bg-blue-500 text-white' : 'bg-gray-300 text-gray-700'} flex items-center justify-center`}>1</div>
+                <div className={`w-8 h-8 rounded-full ${currentPage === 1 ? 'bg-[#12BAE3] text-white' : 'bg-gray-300 text-gray-700'} flex items-center justify-center`}>1</div>
                 <span className="text-sm text-gray-500 mt-2">Introduction</span>
             </div>
             <div className="border-t border-gray-300 flex-1"></div>
             <div className={`flex flex-col items-center ${currentPage === 2 ? 'font-bold' : 'font-normal'}`}>
-                <div className={`w-8 h-8 rounded-full ${currentPage === 2 ? 'bg-blue-500 text-white' : 'bg-gray-300 text-gray-700'} flex items-center justify-center`}>2</div>
+                <div className={`w-8 h-8 rounded-full ${currentPage === 2 ? 'bg-[#12BAE3] text-white' : 'bg-gray-300 text-gray-700'} flex items-center justify-center`}>2</div>
                 <span className="text-sm text-gray-500 mt-2">Referral Details</span>
             </div>
             <div className="border-t border-gray-300 flex-1"></div>
             <div className={`flex flex-col items-center ${currentPage === 3 ? 'font-bold' : 'font-normal'}`}>
-                <div className={`w-8 h-8 rounded-full ${currentPage === 3 ? 'bg-blue-500 text-white' : 'bg-gray-300 text-gray-700'} flex items-center justify-center`}>3</div>
+                <div className={`w-8 h-8 rounded-full ${currentPage === 3 ? 'bg-[#12BAE3] text-white' : 'bg-gray-300 text-gray-700'} flex items-center justify-center`}>3</div>
                 <span className="text-sm text-gray-500 mt-2">Young Person Details</span>
             </div>
             <div className="border-t border-gray-300 flex-1"></div>
             <div className={`flex flex-col items-center ${currentPage === 4 ? 'font-bold' : 'font-normal'}`}>
-                <div className={`w-8 h-8 rounded-full ${currentPage === 4 ? 'bg-blue-500 text-white' : 'bg-gray-300 text-gray-700'} flex items-center justify-center`}>4</div>
+                <div className={`w-8 h-8 rounded-full ${currentPage === 4 ? 'bg-[#12BAE3] text-white' : 'bg-gray-300 text-gray-700'} flex items-center justify-center`}>4</div>
                 <span className="text-sm text-gray-500 mt-2">Eligibility Criteria</span>
             </div>
             <div className="border-t border-gray-300 flex-1"></div>
             <div className={`flex flex-col items-center ${currentPage === 5 ? 'font-bold' : 'font-normal'}`}>
-                <div className={`w-8 h-8 rounded-full ${currentPage === 5 ? 'bg-blue-500 text-white' : 'bg-gray-300 text-gray-700'} flex items-center justify-center`}>5</div>
+                <div className={`w-8 h-8 rounded-full ${currentPage === 5 ? 'bg-[#12BAE3] text-white' : 'bg-gray-300 text-gray-700'} flex items-center justify-center`}>5</div>
                 <span className="text-sm text-gray-500 mt-2">Eligibility Status</span>
             </div>
         </div>
@@ -122,4 +124,4 @@ const ReferralForm: React.FC = () => {
   );
 };
 
-export default ReferralForm;
+export default RgstrnCompForm;
